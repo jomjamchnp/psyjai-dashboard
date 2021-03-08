0c449e3ece09df396f44e93c12bf6b9e24e7551e
chanapat <chanapat.sae@student.mahidol.edu>
2021-03-08 18:12:10 +0700

    edit niddeaw

--- src/page/mood.js
+++ src/page/mood.js
@@ -1,20 +1,23 @@
-import React from "react";
-import {MonthPickerDropdown} from 'react-month-picker-dropdown'
-import 'react-month-picker-dropdown/dist/index.css'
-import YearMonthPicker from 'react-year-month-picker'
+import React,{useState} from "react";
+// import {MonthPickerDropdown} from 'react-month-picker-dropdown'
+// import 'react-month-picker-dropdown/dist/index.css'
+// import YearMonthPicker from 'react-year-month-picker'
 import {CanvasJSChart} from 'canvasjs-react-charts'
 import avatar from '../images/matthew.png'; 
 import '../css/dash.css'
 import 'semantic-ui-css/semantic.css';
 // import DateFnsUtils from "@date-io/date-fns";
-import { MuiPickersUtilsProvider, DatePicker } from "@material-ui/pickers";
-import { Segment,Label,Menu,Input,Image,Icon } from 'semantic-ui-react'
+// import DateFnsUtils from "@date-io/date-fns";
+// import { MuiPickersUtilsProvider, DatePicker } from "@material-ui/pickers";
+import { Segment,Label,Menu,Input,Image,Dropdown } from 'semantic-ui-react'
 import { Col,Row,Button,Grid,Container } from 'react-bootstrap'
 import { FaHome,FaTheaterMasks,FaRegLightbulb,FaRegEdit,FaTree,FaQuestion} from "react-icons/fa";
 import { MdMood } from "react-icons/md";
 import { GrArticle } from "react-icons/gr";
 // import {browserHistory} from 'react-router';
 import { history } from '../history';
+//import ReactMonthPicker from "react-month-picker";
+//import "react-month-picker/css/month-picker.css";
 
 // var Component = React.Component;
 // var CanvasJSReact = require('./canvasjs.react');
@@ -23,8 +26,13 @@ import { history } from '../history';
 class Mood extends React.Component {
   constructor(props) {
     super(props)
-    this.state = { scheduled: null }
+    this.state = { 
+      scheduled: null, 
+      selectedDate : '',
+      setSelectedDate: ''
+    }
     this.onDrop = this.onDrop.bind(this);
+    
   }
   state = { activeItem: 'home' }
   onDrop(picture) {
@@ -36,27 +44,24 @@ class Mood extends React.Component {
     { activeItem: name },
     history.push('/'+name)
   );
-  
   handleChange (m) {
       this.setState({
         scheduled: m
       }, 
       () => console.log(this.state.scheduled))
-  }  
+  } 
+  handleDateChange = (Date) => {
+    this.setState({
+      setSelectedDate : Date
+    });
+    console.log(this.state.setSelectedDate)
+  }; 
 //   redirect(to) {
 //     browserHistory.push({
 //        pathname: to
 //     });
 // } 
   render() {
-    const [selectedDate, setSelectedDate] = React.useState<Date | null>(
-      new Date("2014-08-18T21:11:54")
-    );
-  
-    const handleDateChange = (date: Date | null) => {
-      setSelectedDate(date);
-    };
-    
     const options = {
 			exportEnabled: true,
 			animationEnabled: true,
@@ -81,6 +86,27 @@ class Mood extends React.Component {
 			}]
     }
     const { activeItem } = this.state
+    const month = [
+      { key: 1, text: 'มกราคม', value: 1 },
+      { key: 2, text: 'กุมภาพันธ์', value: 2 },
+      { key: 3, text: 'มีนาคม', value: 3 },
+      { key: 4, text: 'เมษายน', value: 4 },
+      { key: 5, text: 'พฤษภาคม', value: 5 },
+      { key: 6, text: 'มิถุนายน', value: 6 },
+      { key: 7, text: 'กรกฎาคม', value: 7 },
+      { key: 8, text: 'สิงหาคม', value: 8 },
+      { key: 9, text: 'กันยายน', value: 9 },
+      { key: 10, text: 'ตุลาคม', value: 10 },
+      { key: 11, text: 'พฤศจิกายน', value: 11 },
+      { key: 12, text: 'ธันวาคม', value: 12 }
+    ]
+    const year = [
+      { key: 1, text: '2019' ,value:1},
+      { key: 2, text: '2020' ,value:2},
+      { key: 2, text: '2021' ,value:2},
+    ]
+
+
     
     return (
      <div >
@@ -157,15 +183,8 @@ class Mood extends React.Component {
       </Col>
       <Col xs={6} md={8} ls={8} xl={8}>
       <Segment>
-      <DatePicker
-        variant="inline"
-        openTo="year"
-        views={["year", "month"]}
-        label="Year and Month"
-        helperText="Start from year selection"
-        value={selectedDate}
-        onChange={handleDateChange}
-/>
+      <Dropdown clearable options={month} selection />
+      <Dropdown clearable options={year} selection />
       <CanvasJSChart options = {options}/>
       </Segment>
       </Col>
